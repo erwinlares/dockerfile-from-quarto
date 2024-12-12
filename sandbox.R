f18f685ea4691de54d26d0bf5a13418a83de15bf
Erwin Lares <erwin.lares@wisc.edu>
2024-12-12 15:59:36 -0600

    submit_htc() is now functional

--- sandbox.R
+++ sandbox.R
@@ -4,7 +4,7 @@
 library(jsonlite)
 library(glue)
 library(readr)
-library(progress)
+library(tidyverse)
 
 
 
@@ -17,22 +17,46 @@ submit_to_htc <- function(notebook,
                           gitlab_id = "erwin.lares"
 ){
 
-
+#1/10
+print("Step 1 of 10 ... locating .lock file ...") 
+    
 #check that the file exist/renv.lock exist
     if(!file.exists(glue("{getwd()}/renv.lock"))){
-        print("No `renv.lock` file found in current location")
+        print("Error: No `renv.lock` file found in current location")
         return()
     }
 
 # 1/n lock file located 
 
-print("Step 1 of 8 ... locate .lock file ... done")
+    print("Step 1 of 10 ... locating .lock file ... done")
 
-# 2/n extract the code from the notebook
-    knitr::purl(notebook, documentation = 1)
-    print("Step 2 of 8 create .R file ... done")
+# 2/10 extract the code from the notebook
+    
+print("Step 2 of 10 ... creating .R script file ... ")
+    if(!file.exists(notebook)){
+        print("Error: No notebook found in the specified location")
+        return()
+    }
+    
+    if(!str_extract(notebook, "(\\w+)$") %in% c("qmd", "rmd","r", "R")){
+        print("Error: Not a valid file format. Only .qmd or .rmd accepted")
+        return()
+    }
+    
+    if(str_extract(notebook, "(\\w+)$") %in% c("r", "R")){
+        print("Already an .R script")
+    }
+    
+    
+    if(str_extract(notebook, "(\\w+)$") %in% c("qmd","rmd")){
+        knitr::purl(notebook, documentation = 1)
+        print("Step 2 of 10 ... creating .R script file ... done")
+    }
+        
 
 # 3/10 write the Dockerfile
+    
+    print("Step 3 of 10 ... creating Docker file ... ")
     r_version <- R.Version()
     FROM_line <- glue::glue(
         "FROM rocker/r-ver:{glue::glue('{r_version$major}.{r_version$minor}')}")
@@ -56,40 +80,70 @@ print("Step 1 of 8 ... locate .lock file ... done")
                        file = "Dockerfile",
                        append = TRUE)
     
-    print("Step 2 of 8 ... create Dockerfile ... done")
+    print("Step 3 of 10 ... creating Dockerfile ... done")
 
-# Build the image 
+# 4/10 Build the image 
+    print("Step 4 of 10 ... creating container image ... ")
 # commenting this line out so I don't have tons of images while debugging
     #    system(glue("podman build -t {gitlab_registry}/{gitlab_id}/{gitlab_repo} ."))
 
-    print("Step 3 of 8 ... create container image ... done")
+    print("Step 4 of 10 ... creating container image ... done")
 
-# Authenticate with the gitlab registry
-    
+# 5/10 Authenticate with the gitlab registry
+    print("Step 5 of 10 ... logging in to GitLab registry ... ")
     #system(glue("podman login {gitlab_registry}"))
-    print("Step 4 of 8 ... login to GitLab registry ... done")
+    print("Step 5 of 10 ... logging in  to GitLab registry ... done")
 
-# Push container image to gitlab registry
+# 6/10 Push container image to gitlab registry
 
+    print("Step 6 of 10 ... pushing container image to GitLab registry ... ")
     #system(glue("podman push {gitlab_registry}/{gitlab_id}/{gitlab_repo}"))
-    print("Step 5 of 8 ... push container image to GitLab registry ... done")
+    print("Step 6 of 10 ... pushing container image to GitLab registry ... done")
 
-# 6/10Create submit file 
-    print("Step 6 of 8 ... create submit file ... done")
-
-# 7/10Create executable file 
-    print("Step 7 of 8 ... create executable file ... done")
-
+# 7/10 Create submit file 
+    print("Step 7 of 10 ... creating submit file ... ")
+    #creating the submit file line-by-line
+    print("Step 7 of 10 ... creating submit file ... done")
+# 8/10 Create executable file 
+    print("Step 8 of 10 ... creating executable file ... ")
+    #creating .sh file line-by-line 
+    print("Step 8 of 10 ... creating executable file ... done")
     
-# Copy over .R, data, .sub, and .sh files to CHTC 
+# 9/10 Copy over .R, data, .sub, and .sh files to CHTC 
 # does too! work 
     
-# 8/10 COpy files over to chtc
     
+    print("Step 9 of 10 ... uploading .R, data, .sub, and .sh to CHTC ... ")
     system(glue("scp analysis.R data.csv analysis.sub analysis.sh lares@ap2001.chtc.wisc.edu:/home/lares"))
     
-    print("Step 8 of 8 ... copy .R, data, .sub, and .sh to CHTC ... done")
+    print("Step 9 of 10 ... uploading .R, data, .sub, and .sh to CHTC ... done")
 
+#10/10  give concise directions to what to do next 
+    
+    print("Step 10/10 ... ")
 
 }
-# 
+
+submit_to_htc("analysis.qmd")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(str_extract(notebook, "(\\w+)$") %in% c("qmd","rmd")){
+    knitr::purl(notebook, documentation = 1)
+    print("Step 2 of 10 ... creating .R script file ")
+}
+
+
+
+
