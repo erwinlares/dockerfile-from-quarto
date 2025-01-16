3a03edcd3e5ae2bb8553c89fe5b890ce69a0add4
Erwin Lares <erwin.lares@wisc.edu>
2025-01-16 15:39:49 -0600

    recommendations from Amber Lim

---
+++ suggestion_amber.R
@@ -0,0 +1,5 @@
+#1. have a job that sub divides the whole dataset into the corresponding subdatasets
+
+#2 leveraging arguments in the submit file to put out specific data from the datasets. 
+
+#check this: https://chtc.cs.wisc.edu/uw-research-computing/htc-passing-arguments
